ee059e002306376dd226dc912d6f1758aff4795a
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-09-27 23:52:15 +0200

    Notificaciones

--- FreeBitcoin Captcha/ViewController.swift
+++ FreeBitcoin Captcha/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import WebKit
 import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     //Elementos Pantalla
     @IBOutlet var miWebKit: WKWebView!
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().delegate = self
         miWebKit.load(enlace)
     }
 
@@ -31,8 +32,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Notificaciones
+    func Notificar(){
+        //Trigger Notificación
+        let triggerN = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        
+        //Contenido Notificación
+        let contenidoN = UNMutableNotificationContent()
+        contenidoN.title = "Añade Bitcoins 💰"
+        contenidoN.body = "Ya ha pasado más de 1⌚️Hora, vuelve para ganarte unos 🤑 Bitcoins 🤑"
+        contenidoN.sound = UNNotificationSound.default()
+        
+        //Request Notificación
+        let requestN = UNNotificationRequest(identifier: "FreeBitcoin Captcha", content: contenidoN, trigger: triggerN)
+        
+        //Elimina notificaciones pendientes
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        //Envia la notificacion
+        UNUserNotificationCenter.current().add(requestN) {(error) in
+            if let error = error {
+                print("Se ha producido un error: \(error)")
+            }
+        }
+        
+        
+    }
+    
+        //Mostrar notificacion incluso dentro de la app
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            completionHandler([.alert, .sound])
+        }
 
     //Botones inferiores
+        //Inicia Cuenta Atras
+        @IBAction func IniciarCuenta(_ sender: UIBarButtonItem) {
+            print("Se procede a iniciar a cuenta atras")
+            Notificar()
+        }
+    
         //Cancelar Web Carga
         @IBAction func CancelarWeb(_ sender: UIBarButtonItem) {
             print("Se ha cancelado cargar contenido")
@@ -44,11 +83,15 @@ class ViewController: UIViewController {
             print("Se va a proceder a abrir la Web")
         }
     
-    
         //Recargar Web
         @IBAction func RecargarWeb(_ sender: UIBarButtonItem) {
             print("Recargando Web")
             miWebKit.reload()
+        }
+    
+        //Cancela Cuenta Atras
+        @IBAction func CancelarCuenta(_ sender: UIBarButtonItem) {
+            print("Se procede a cancelar la cuenta atras")
         }
 }
 
