3b93291c5c10c93c8f9261d5c47836edf8a79f9e
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-09-27 23:15:21 +0200

    App funcional con Web FreeBitcoins

--- FreeBitcoin Captcha/ViewController.swift
+++ FreeBitcoin Captcha/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import WebKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    //Elementos Pantalla
+    @IBOutlet var miWebKit: WKWebView!
+    @IBOutlet var contador: UILabel!
+    @IBOutlet var progresso: UIProgressView!
+    
+    //Variables
+    let enlace = URLRequest(url: URL(string: "https://www.freebitco.in/?op=home")!)
+    var tiempo = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        miWebKit.load(enlace)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Botones inferiores
+        //Cancelar Web Carga
+        @IBAction func CancelarWeb(_ sender: UIBarButtonItem) {
+            print("Se ha cancelado cargar contenido")
+            miWebKit.stopLoading()
+        }
+    
+        //Abrir Web Externa
+        @IBAction func AbrirWeb(_ sender: UIBarButtonItem) {
+            print("Se va a proceder a abrir la Web")
+        }
+    
+    
+        //Recargar Web
+        @IBAction func RecargarWeb(_ sender: UIBarButtonItem) {
+            print("Recargando Web")
+            miWebKit.reload()
+        }
 }
 
