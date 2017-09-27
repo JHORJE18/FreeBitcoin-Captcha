2a9f94bc8f4ce1f1fd15fc8be27c85f82d34f085
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-09-27 21:33:28 +0200

    Initial Commit

---
+++ FreeBitcoin Captcha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreeBitcoin Captcha
+//
+//  Created by Jorge Lopez Gil on 27/9/17.
+//  Copyright © 2017 Jorge Lopez Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
